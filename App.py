a4a3ee2a9f8f61955204dce548bd393471513d3a
Sebastian Olsson <sebas-o@online.no>
2024-02-21 13:17:08 +0100

    fixed something, idk what

--- App.py
+++ App.py
@@ -1,12 +1,26 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
+    if request.method == 'POST':
+        try:
+            desc = request.form['desc']
+
+            with sqlite3.connect('./database.sqlite') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO todo (description) VALUES (?)",(desc,))
+
+                con.commit()
+        except:
+            con.rollback()
+
+        finally:
+            con.close()
+            return redirect(url_for('home'))
+
     con = sqlite3.connect("database.sqlite")
     con.row_factory = sqlite3.Row
 
@@ -17,33 +31,26 @@ def home():
     con.close()
     return render_template('index.html', rows=rows)
 
-@app.route("/addtask")
-def addtask():
-    return render_template('addtask.html')
-
-@app.route("/addta", methods = ["POST", "GET"])
-def addta():
-    if request.method == 'POST':
-        try:
-            desc = request.form['desc']
-
-            with sqlite3.connect('./database.sqlite') as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO todo (description) VALUES (?)",(desc,))
-
-                con.commit()
-                msg = "Record successfully added to database"
-        except:
-            con.rollback()
-            msg = "Error in the INSERT"
-
-        finally:
-            con.close()
-            return render_template('result.html',msg=msg)
-
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+@app.route("/toggle_completed/<int:task_id>", methods=["POST"])
+def toggle_completed(task_id):
+    with sqlite3.connect('./database.sqlite') as con:
+        cur = con.cursor()
+        cur.execute("UPDATE todo SET completed = NOT completed WHERE id = ?", (task_id,))
+        con.commit()
+    return redirect(url_for('home'))
+
+@app.route("/remove_task/<int:task_id>", methods=["POST"])
+def remove_task(task_id):
+    with sqlite3.connect('./database.sqlite') as con:
+        cur = con.cursor()
+        cur.execute("DELETE FROM todo WHERE id = ?", (task_id,))
+        con.commit()
+    return redirect(url_for('home'))
+
 
 
 if __name__ == "__main__": 
