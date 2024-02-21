da18aedfdee19e06d6f3b822dc1976df512b9e09
Sebastian Olsson <sebas-o@online.no>
2024-02-21 11:32:11 +0100

    you can add stuff to the DB!

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -10,6 +12,26 @@ def home():
 @app.route("/addtask")
 def addtask():
     return render_template('addtask.html')
+
+@app.route("/addta", methods = ["POST", "GET"])
+def addta():
+    if request.method == 'POST':
+        try:
+            desc = request.form['desc']
+
+            with sqlite3.connect('./database.sqlite') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO todo (description) VALUES (?)",(desc,))
+
+                con.commit()
+                msg = "Record successfully added to database"
+        except:
+            con.rollback()
+            msg = "Error in the INSERT"
+
+        finally:
+            con.close()
+            return render_template('result.html',msg=msg)
 
 @app.route("/login")
 def login():
--- create_table.py
+++ create_table.py
@@ -1,5 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('./database.db')
+conn = sqlite3.connect('./database.sqlite')
 
-conn.execute('CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT NOT NULL, completed BOOLEAN NOT NULL DEFAULT false)')
+# conn.execute('CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT NOT NULL, completed BOOLEAN DEFAULT false)')
+conn.execute('INSERT INTO todo (description) VALUES ("test")')
+
+conn.commit()
+
+conn.close()
